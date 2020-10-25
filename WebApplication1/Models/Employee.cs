c8c68533ff6e311811f669d4117611c50984c04f
ShoLap <shoaibk9100@gmail.com>
2020-10-25 15:49:21 +0500

    Add project files.

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+    
+
+
+    }
+}
---
+++ WebApplication1/Models/connectionDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class connectionDB
+    {
+        public static SqlConnection my_SqlCons;
+
+        public static SqlConnection GetCons()
+        {
+            if (my_SqlCons == null) {
+                my_SqlCons = new SqlConnection();
+                my_SqlCons.ConnectionString = ConfigurationManager.ConnectionStrings["crudDb_ins"].ConnectionString;
+                my_SqlCons.Open();
+            }
+            return my_SqlCons;
+        }
+    }
+}
